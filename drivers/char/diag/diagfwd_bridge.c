8c86f68b0d112f2fadd1b2e73573eae2679d0049
Vaisakh Murali <mvaisakh@statixos.com>
2021-05-24 12:52:48 +0530

    Revert "drivers: char: Drop reference to diag_unregister_hsic"
    This reverts commit 15141e393b70f5fc4e343b7ba3ed475aecd84a83.

--- drivers/char/diag/diagfwd_bridge.c
+++ drivers/char/diag/diagfwd_bridge.c
@@ -330,6 +330,8 @@ void diag_register_with_bridge(void)
 
 void diag_unregister_bridge(void)
 {
-	if (IS_ENABLED(CONFIG_MHI_BUS))
+	if (hsic_interface_active)
+		diag_unregister_hsic();
+	else if (IS_ENABLED(CONFIG_MHI_BUS))
 		diag_unregister_mhi();
 }
